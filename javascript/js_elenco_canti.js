ff8fd74561d3c9117b84f534d28cf270f1bd5ff0
mrparsing <fralindi5@gmail.com>
2024-10-20 22:17:44 +0200

    Update js_elenco_canti.js

--- javascript/js_elenco_canti.js
+++ javascript/js_elenco_canti.js
@@ -25,7 +25,7 @@ fetch('../db/canti.json')
             const detailsDiv = document.createElement('div');
             detailsDiv.className = 'canto-details';
             detailsDiv.style.display = 'none';
-            detailsDiv.innerHTML = `<strong>Autore:</strong> ${canto.autore} | <strong>Tipologia:</strong> ${canto.tipologia} | <strong>Tempo: </strong> ${canto.tempo}`;
+            detailsDiv.innerHTML = `<strong>Autore:</strong> ${canto.autore} | <strong>Tipologia:</strong> ${canto.tipologia}`;
 
             const linksDiv = document.createElement('div');
             const ascoltaLink = document.createElement('a');
