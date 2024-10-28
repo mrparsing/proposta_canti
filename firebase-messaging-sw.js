b1e295fbac80243e1d36b1f625b0f98700217463
mrparsing <fralindi5@gmail.com>
2024-10-28 12:08:59 +0100

    notifiche

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js");
 
+// Configura Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAKG4mi3DGhomBo989PHUilHYW-PF-akzI",
     authDomain: "proposta-canti-13252.firebaseapp.com",
@@ -12,14 +13,15 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('Messaggio ricevuto in background:', payload);
+// Gestisce le notifiche in arrivo
+messaging.onBackgroundMessage(payload => {
+    console.log("Messaggio in background ricevuto:", payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/firebase-logo.png'  // Cambia con il percorso della tua icona
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
