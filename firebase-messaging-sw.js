7815b094886bf1f27285b90d7d36e00faf4e1bd1
mrparsing <fralindi5@gmail.com>
2024-10-28 14:07:21 +0100

    fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,10 @@
-// Importa i moduli Firebase
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js");
+// firebase-messaging-sw.js
 
-// Configura Firebase
-const firebaseConfig = {
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js');
+
+// Configurazione di Firebase per il service worker
+firebase.initializeApp({
     apiKey: "AIzaSyAKG4mi3DGhomBo989PHUilHYW-PF-akzI",
     authDomain: "proposta-canti-13252.firebaseapp.com",
     projectId: "proposta-canti-13252",
@@ -11,22 +12,16 @@ const firebaseConfig = {
     messagingSenderId: "643920970942",
     appId: "1:643920970942:web:5351c41cc6068558a48aef",
     measurementId: "G-1W0M5CGBGT"
-};
+});
 
-// Inizializza Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Ottieni un'istanza di Firebase Messaging
 const messaging = firebase.messaging();
 
-// Gestisce le notifiche in arrivo in background
+// Listener per gestire le notifiche
 messaging.onBackgroundMessage((payload) => {
     console.log("Messaggio in background ricevuto:", payload);
-
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
     };
-
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
