1b7c2544be21c6fa4dc784058ff670c35261f2fc
mrparsing <fralindi5@gmail.com>
2024-10-28 15:35:33 +0100

    l

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,29 +1,44 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js";
+
 console.log("test sw");
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyAKG4mi3DGhomBo989PHUilHYW-PF-akzI",
     authDomain: "proposta-canti-13252.firebaseapp.com",
     projectId: "proposta-canti-13252",
     storageBucket: "proposta-canti-13252.appspot.com",
     messagingSenderId: "643920970942",
     appId: "1:643920970942:web:5351c41cc6068558a48aef",
-});
+};
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
 
-// Listener per gestire le notifiche
-messaging.onBackgroundMessage((payload) => {
-    console.log("Messaggio in background ricevuto:", payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-    };
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
+function requestPermission() {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+            console.log('Notification permission granted');
+            const app = initializeApp(firebaseConfig)
+            // Retrieve an instance of Firebase Messaging so that it can handle background
+            // messages.
+            const messaging = getMessaging(app)
+
+            getToken(messaging, { vapidKey: 'BNWgH3H0wYtTullS6HQWgW9Oc79B6_d3i-wNP--UwP_-skGtA4VR3w8U-c0K7bD6jO63-vykpBVYBCQA0sQjxlM' }).then((currentToken) => {
+                if (currentToken) {
+                    console.log('currentToken', currentToken);
+                } else {
+                    console.log('Can not get token');
+                }
+            });
+        } else {
+            console.log('Do not have permission');
+        }
+    })
+}
+
+requestPermission()
